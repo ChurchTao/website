162d37f6dba25ebc55fb0931f28d07e5b3730b62
taojiacheng <swkzymlyy@gmail.com>
2023-02-07 15:31:08 +0800

    i18n and search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,17 +39,19 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/churchTao/website/tree/master",
+          routeBasePath: "/",
+          path: "docs/",
+          exclude: ["docs/**", "**/_*.{md,mdx}"],
+          editCurrentVersion: true,
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/churchTao/website/tree/master",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -61,13 +63,21 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
+      metadata: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no",
+        },
+        { name: "keyword", content: "lanaya,clipboard,tauri,macos,linux,windows,desktop" },
+      ],
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
+        title: "Lanaya",
         logo: {
-          alt: "My Site Logo",
+          alt: "churchTao",
           src: "img/logo.svg",
+          style: { borderRadius: "50%" },
         },
         items: [
           {
@@ -77,8 +87,9 @@ const config = {
             label: "Tutorial",
           },
           { to: "/blog", label: "Blog", position: "left" },
+          { type: "localeDropdown", position: "right" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/churchTao/Lanaya",
             label: "GitHub",
             position: "right",
           },
@@ -93,23 +104,6 @@ const config = {
               {
                 label: "Tutorial",
                 to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
               },
             ],
           },
@@ -132,6 +126,24 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // Algolia 提供的应用 ID
+        appId: "YOUR_APP_ID",
+        //  公开 API 密钥：提交它没有危险
+        apiKey: "YOUR_SEARCH_API_KEY",
+        indexName: "YOUR_INDEX_NAME",
+        // 可选：见下文
+        contextualSearch: true,
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+        // Optional: Algolia search parameters
+        searchParameters: {},
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
       },
     }),
 };
